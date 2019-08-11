b9d36327956da3e2105e3d784fb2e57e6f353633
FilippoBovo <bovo.filippo@gmail.com>
2019-08-11 16:16:59 +0100

    Update setup.py and create MANIFEST.in for publish the package on PyPI.

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import os
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import numpy.distutils.misc_util
 
 
-# C files
-source_dir = "c"
-source_files = ["_robustats.c", "robustats.c", "base.c"]
-source_paths = [os.path.join(source_dir, file) for file in source_files]
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 
 setup(
     name = 'robustats',
-    version = '0.1',
+    version = '0.1.2',
     description = 'Robustats is a Python library for high-performance '
                   'computation of robust statistical estimators.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
     url='https://github.com/FilippoBovo/robustats',
-    download_url='https://github.com/FilippoBovo/robustats/archive/v0.1.tar.gz',
+    download_url='https://github.com/FilippoBovo/robustats/archive/v0.1.2.tar.gz',
     author = 'Filippo Bovo',
     author_email = 'bovo.filippo@gmail.com',
     license='MIT',
@@ -28,7 +28,7 @@ setup(
     ext_modules=[
         Extension(
             name="_robustats",
-            sources=source_paths
+            sources=["c/_robustats.c", "c/robustats.c", "c/base.c"]
         )
     ],
     include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
